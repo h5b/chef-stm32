25b956425e1df734d5b23e71a190032ddf28c978
Sebastian Trahm <basti@schleifi.com>
2014-12-15 21:34:23 +0100

    Install OpenOCD to flash the board.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,22 @@
+cache_path = Chef::Config[:file_cache_path]
+configure_flags = node[:openocd][:configure_flags].join(" ")
+
 node['stm32']['packages'].each do |p|
   package p
+end
+
+remote_file File.join(cache_path, "openocd-#{node[:openocd][:version]}.tar.gz") do
+  source "#{node[:openocd][:mirror]}#{node[:openocd][:version]}/openocd-#{node[:openocd][:version]}.tar.gz"
+  notifies :run, "bash[install_openocd]", :immediately
+end
+
+bash "install_openocd" do
+  user "root"
+  cwd cache_path
+  code <<-EOH
+    tar xfz openocd-#{node[:openocd][:version]}.tar.gz
+    cd openocd-#{node[:openocd][:version]}
+    ./configure #{configure_flags}
+    make && make install
+    EOH
 end
